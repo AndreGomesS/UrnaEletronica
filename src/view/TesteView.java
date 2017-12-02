89535d8dd1b3f29e32d15fe8b6a7affeb0ba9da4
Elias Gonçalves <si.elias.ti@gmail.com>
2017-12-02 11:03:42 -0200

    Remoção de linha de comentário inútil.

--- src/view/TesteView.java
+++ src/view/TesteView.java
@@ -34,11 +34,7 @@ public class TesteView {
         System.out.println(
             "Notifica View: " +
             TesteController.criarTeste(dadosView)
-        );
-        
-        // Simular a escrita:
-       
-        
+        );        
     }
     
 }
